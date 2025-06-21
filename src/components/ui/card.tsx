f13516de6fe733da1507eb63afbd05eb587651a1
Riza Irawan <riza.dvnl@gmail.com>
2025-06-21 22:25:01 +0000

    ubah card menjadi memiliki shadow

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -10,7 +10,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg bg-card text-card-foreground", // Removed border class
+      "rounded-lg bg-card text-card-foreground shadow-md transition-shadow hover:shadow-lg",
       className
     )}
     {...props}
